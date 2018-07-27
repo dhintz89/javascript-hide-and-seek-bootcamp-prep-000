5f4a8a01b2a040025a87301fb72eb3d2b722bc80
Dan Hintz <dhintz@live.com>
2018-07-27 00:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  for (var i=0; i<rankedLists.length; i++) {
+    rankedLists[i].innerHTML = (parseInt(rankedLists[i],10)+1).toString();
+  }
+}
+
+function deepestChild() {
+  
+}
